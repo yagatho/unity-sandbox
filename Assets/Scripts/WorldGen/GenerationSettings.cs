5cfd1df7660c3578ee7033a68344af66afff7c62
Szymon Grucela <szymonmlbb@gmail.com>
2025-04-17 18:09:50 +0200

    Made distinction between vert based generation and quad based generation

--- Assets/Scripts/WorldGen/GenerationSettings.cs
+++ Assets/Scripts/WorldGen/GenerationSettings.cs
@@ -7,7 +7,11 @@ namespace Project.World
         //Chunk size is the size of the chunk in world units
         public const float chunkSize = 32;
 
+        //Quad based
         //Chunk size in quads x by x
-        public const int quads = 15;
+        public const int quads = 6;
+
+        //Vert based
+        public const int verts = 7;
     }
 }
--- Assets/Scripts/WorldGen/ProceduralGeneration.cs
+++ Assets/Scripts/WorldGen/ProceduralGeneration.cs
@@ -72,52 +72,8 @@ namespace Project.World
                 return;
             }
 
-            myMesh.Clear();
-            int quads = GenerationSettings.quads;
-
             //Generate the mesh
-            Vector3[] vertices = new Vector3[quads * quads * 4];
-            Vector2[] uvs = new Vector2[quads * quads * 4];
-            int vertexIndex = 0;
-            int triangleIndex = 0;
-            int uvIndex = 0;
-            int quadIndex = 0;
-
-            int[] triangles = new int[quads * quads * 6];
-
-            for (int y = 0; y < quads; y++)
-            {
-                for (int x = 0; x < quads; x++)
-                {
-                    Vector3 baseVert = new Vector3(GenerationSettings.chunkSize / quads * x, 0, GenerationSettings.chunkSize / quads * y);
-
-                    vertices[vertexIndex++] = new Vector3(0, 0, 0) + baseVert;
-                    vertices[vertexIndex++] = new Vector3(0, 0, GenerationSettings.chunkSize / quads) + baseVert;
-                    vertices[vertexIndex++] = new Vector3(GenerationSettings.chunkSize / quads, 0, 0) + baseVert;
-                    vertices[vertexIndex++] = new Vector3(GenerationSettings.chunkSize / quads, 0, GenerationSettings.chunkSize / quads) + baseVert;
-
-
-                    Vector2 baseUv = new Vector2((float)x / quads, (float)y / quads);
-
-                    uvs[uvIndex++] = new Vector2(0, 0) + baseUv;
-                    uvs[uvIndex++] = new Vector2(0, quads / quads) + baseUv;
-                    uvs[uvIndex++] = new Vector2(quads / quads, 0) + baseUv;
-                    uvs[uvIndex++] = new Vector2(quads / quads, quads / quads) + baseUv;
-
-                    triangles[triangleIndex++] = 0 + 4 * quadIndex;
-                    triangles[triangleIndex++] = 1 + 4 * quadIndex;
-                    triangles[triangleIndex++] = 2 + 4 * quadIndex;
-                    triangles[triangleIndex++] = 2 + 4 * quadIndex;
-                    triangles[triangleIndex++] = 1 + 4 * quadIndex;
-                    triangles[triangleIndex++] = 3 + 4 * quadIndex;
-
-                    quadIndex++;
-                }
-            }
-
-            myMesh.vertices = vertices;
-            myMesh.triangles = triangles;
-            myMesh.uv = uvs;
+            MeshUtils.VertBasedMesh(myMesh);
 
             myMesh.RecalculateNormals();
             myMesh.RecalculateBounds();
